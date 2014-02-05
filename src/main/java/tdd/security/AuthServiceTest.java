07137ad3e75efeaf2f85f96af1d0c6f2b800051c
{hongseongmin} <{hnsnmn@gmail.com}>
2014-02-05 11:05:27 +0900

    AuthServiceTest를 생성 - nothing() 메소드를 추가해서 테스트 성공

---
+++ src/main/java/tdd/security/AuthServiceTest.java
@@ -0,0 +1,20 @@
+package tdd.security;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 2. 5.
+ * Time: 오전 10:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class AuthServiceTest {
+	@Test
+	public void nothing() {
+
+	}
+}
+
+
+
